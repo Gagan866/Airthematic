2361d565134fa47d88162d204f049215e16df53b
Gagan866 <gd0363264@gmail.com>
2025-03-05 12:28:54 +0530

    changed

---
+++ methods.java
@@ -0,0 +1,10 @@
+package Airthematic;
+
+interface method {
+        void add();
+
+        void sub();
+
+        void mul();
+    }
+
---
+++ operations.java
@@ -0,0 +1,14 @@
+package Airthematic;
+
+public class operations implements method{
+        public void add(){
+            System.out.println("Addition");
+        }
+        public void sub(){
+            System.out.println("Subtraction");
+        }
+        public void mul(){
+            System.out.println("Multiplication");
+        }
+    }
+
